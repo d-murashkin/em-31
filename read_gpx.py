84fd2627d5bb30faeca30e388b023066db9dc414
Dmitrii Murashkin <murdm@ya.ru>
2019-04-24 05:38:19 +0200

    [upd] cmd script

--- read_gpx.py
+++ read_gpx.py
@@ -1,6 +1,8 @@
 """ Read .gpx files produced by GPS devices (like Garmin GPSMAP64s)
     and convert into a .csv file.
 """
+import argparse
+import sys
 
 import pandas as pd
 import datetime
@@ -17,9 +19,16 @@ def read_gpx(inp_file, time_format='%Y-%m-%dT%H:%M:%SZ'):
     return data
 
 
-
 if __name__ == '__main__':
-    inp_file = '/home/dm/work/data/tryoshnikov/EM/gps1/Track_2019-03-30 102146.gpx'
-    out_file = '/home/dm/gps_test.csv'
-    data = read_gpx(inp_file)
-    data.to_csv(out_file)
+    parser = argparse.ArgumentParser(description='Convert *.gpx files created by GPS navigators into *.csv files. Input and output files should be specified with -i and -o options.')
+    parser.add_argument('-i', help='input filename')
+    parser.add_argument('-o', help='output filename')
+    args = parser.parse_args()
+    if not args.i:
+        print('Please, specify input file with -i option.')
+        sys.exit()
+    if not args.o:
+        print('Please, specify output file with -o option.')
+
+    data = read_gpx(args.i)
+    data.to_csv(args.o)
