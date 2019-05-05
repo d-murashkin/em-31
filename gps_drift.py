441e2f34ac12620a499c4646659295d5723b8941
Dmitrii Murashkin <murdm@ya.ru>
2019-05-05 18:01:30 +0200

    [upd] any number of input tracks

--- gps_drift.py
+++ gps_drift.py
@@ -38,35 +38,38 @@ def estimate_drift(gps_data):
     return drift
 
 
-def read_tracks(track_1_file, track_2_file, frequency='1S'):
+def interpolate_tracks(track_file_list, frequency='1S'):
     """ Function creates a pandas DataFrame with both trackes interpolated for given frequency.
         Default frequency is one second.
         This means that there are GPS two coordinates for each second.
     """
-    """ Read GPS tracks """
-    track_1 = pd.read_csv(track_1_file)
-    track_2 = pd.read_csv(track_2_file)
-    track_1['time'] = track_1.time.apply(__detect_time)
-    track_2['time'] = track_2.time.apply(__detect_time)
-    track_1.index = track_1.time
-    track_2.index = track_2.time
-    track_1 = track_1.filter(['lat', 'lon', 'time'])
-    track_2 = track_2.filter(['lat', 'lon', 'time'])
+    """ Read and join GPS tracks. """
+    tracks = [read_track(item) for item in track_file_list]
+    joined_tracks = pd.concat(tracks, axis='columns', join='outer', keys=np.arange(1, len(tracks) + 1))
 
-    """ Create a new pandas DataFrame and interpolate both GPS tracks. """
-    start_time = min(track_1.time[0], track_2.time[0])
-    end_time = max(track_1.time[track_1.shape[0] - 1], track_2.time[track_2.shape[0] - 1])
+    """ Create a new pandas DataFrame. """
+    start_time = min([item.time[0] for item in tracks])
+    end_time = max([item.time[item.shape[0] - 1] for item in tracks])
     track = pd.DataFrame(pd.date_range(start=start_time, end=end_time, freq=frequency))
     track.columns = ['time']
-    track = track.join(track_1, on='time', how='outer', rsuffix='1')
-    track = track.join(track_2, on='time', how='outer', rsuffix='2', lsuffix='1')
-    track = track.drop(['time1', 'time2'], axis=1)
-    track['lat1'].interpolate(method='linear', inplace=True)
-    track['lon1'].interpolate(method='linear', inplace=True)
-    track['lat2'].interpolate(method='linear', inplace=True)
-    track['lon2'].interpolate(method='linear', inplace=True)
+
+    """ Interpolate all GPS tracks. """
+    track = track.join(joined_tracks, on='time', how='outer')
+    track = track.drop(columns=[(i + 1, 'time') for i, _ in enumerate(tracks)])
+    for i, _ in enumerate(tracks):
+        track[(i + 1, 'lat')].interpolate(method='linear', inplace=True)
+        track[(i + 1, 'lon')].interpolate(method='linear', inplace=True)
     track.index = track.time
     track.dropna(axis='index', how='any', inplace=True)
+    return track
+
+
+def read_track(track_file):
+    """ Read GPS track """
+    track = pd.read_csv(track_file)
+    track['time'] = track.time.apply(__detect_time)
+    track.index = track.time
+    track = track.filter(['lat', 'lon', 'time'])
     return track
 
 
@@ -96,20 +99,16 @@ if __name__ == '__main__':
     inp_file_1 = 'gps_1_orange.csv'
     inp_file_2 = 'gps_2_blue.csv'
     inp_file_3 = 'gps_3_yellow.csv'
-    gps_track = pd.read_csv(inp_fld + inp_file_1)
+#    gps_track = pd.read_csv(inp_fld + inp_file_1)
 #    drift = estimate_drift(gps_track)
 #    print(drift.head())
+    track = interpolate_tracks([inp_fld + inp_file_1, inp_fld + inp_file_2])
+    print(track.columns)
     from matplotlib import pyplot as plt
     plt.figure()
-    for item, col in zip([inp_file_1, inp_file_2, inp_file_3], ['orange', 'blue', 'yellow']):
-        data = pd.read_csv(inp_fld + item)
-        plt.scatter(data.lon, data.lat, c=col)
-#    plt.show()
-    track = read_tracks(inp_fld + inp_file_1, inp_fld + inp_file_2)
-    print(track.columns)
-    plt.figure()
-    plt.plot(track.lat1 - track.lat2)
-    plt.plot(track.lon1 - track.lon2)
+    plt.plot(track[(1, 'lon')], track[(1, 'lat')], color='orange')
+    plt.plot(track[(2, 'lon')], track[(2, 'lat')], color='blue')
+    plt.plot((track[(1, 'lon')] + track[(2, 'lon')]) / 2., (track[(1, 'lat')] + track[(2, 'lat')]) / 2., color='green')
     plt.show()
 
     '''
