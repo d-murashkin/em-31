0c938f4011eda78a8526ffd1849fd907d60f3bf7
Dmitrii Murashkin <murdm@ya.ru>
2019-05-05 16:20:11 +0200

    [new] drift every second

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+Ice Floe Survay
 Here there are some script for EM-31 data processing.
 * snow and ice thickness estimate
 * read .gpx files from GPS devices
+* read .R31 files produced with EM-31
 * ice floe drift correction
 * EM-31 calibration
 * sea ice thickness field map
--- gps_drift.py
+++ gps_drift.py
@@ -1,4 +1,10 @@
 
+""" Drift correction with additional GPS trackers.
+"""
+import numpy as np
+import pandas as pd
+
+
 def estimate_drift(gps_data):
     """ Input is expected to be a pandas dataframe with lat, lon, and time columns
     """
@@ -31,15 +37,89 @@ def estimate_drift(gps_data):
     drift['time'] = gps_data['time']
     return drift
 
-if __name__ == '__main__':
 
-    inp_file_1 = ''
-    inp_file_2 = ''
-    inp_file = '/home/dm/work/data/tryoshnikov/EM/gps1/Track_2019-03-30 102146.gpx'
-    gps_track = read_gpx(inp_file)
-    drift = estimate_drift(gps_track)
-    print(drift.head())
-#    from matplotlib import pyplot as plt
-#    plt.figure()
-#    plt.scatter(drift['d_lat'], drift['d_lon'])
+def read_tracks(track_1_file, track_2_file, frequency='1S'):
+    """ Function creates a pandas DataFrame with both trackes interpolated for given frequency.
+        Default frequency is one second.
+        This means that there are GPS two coordinates for each second.
+    """
+    """ Read GPS tracks """
+    track_1 = pd.read_csv(track_1_file)
+    track_2 = pd.read_csv(track_2_file)
+    track_1['time'] = track_1.time.apply(__detect_time)
+    track_2['time'] = track_2.time.apply(__detect_time)
+    track_1.index = track_1.time
+    track_2.index = track_2.time
+    track_1 = track_1.filter(['lat', 'lon', 'time'])
+    track_2 = track_2.filter(['lat', 'lon', 'time'])
+
+    """ Create a new pandas DataFrame and interpolate both GPS tracks. """
+    start_time = min(track_1.time[0], track_2.time[0])
+    end_time = max(track_1.time[track_1.shape[0] - 1], track_2.time[track_2.shape[0] - 1])
+    track = pd.DataFrame(pd.date_range(start=start_time, end=end_time, freq=frequency))
+    track.columns = ['time']
+    track = track.join(track_1, on='time', how='outer', rsuffix='1')
+    track = track.join(track_2, on='time', how='outer', rsuffix='2', lsuffix='1')
+    track = track.drop(['time1', 'time2'], axis=1)
+    track['lat1'].interpolate(method='linear', inplace=True)
+    track['lon1'].interpolate(method='linear', inplace=True)
+    track['lat2'].interpolate(method='linear', inplace=True)
+    track['lon2'].interpolate(method='linear', inplace=True)
+    track.index = track.time
+    track.dropna(axis='index', how='any', inplace=True)
+    return track
+
+
+def modify_tracks(track_1, track_2):
+    """ Function for track correction.
+        The distance between GPS trackers does not change during the measurement time.
+        Therefore both tracks can be smoothened decreasing the coordinate error.
+    """
+    pass
+
+
+def filter_outliers(track):
+    pass
+
+
+def __detect_time(x):
+    return pd.datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def __new_point(t):
+    pass
+
+
+if __name__ == '__main__':
+    pass
+    inp_fld = '/home/dm/work/data/tryoshnikov/EM/04-26/gps/'
+    inp_file_1 = 'gps_1_orange.csv'
+    inp_file_2 = 'gps_2_blue.csv'
+    inp_file_3 = 'gps_3_yellow.csv'
+    gps_track = pd.read_csv(inp_fld + inp_file_1)
+#    drift = estimate_drift(gps_track)
+#    print(drift.head())
+    from matplotlib import pyplot as plt
+    plt.figure()
+    for item, col in zip([inp_file_1, inp_file_2, inp_file_3], ['orange', 'blue', 'yellow']):
+        data = pd.read_csv(inp_fld + item)
+        plt.scatter(data.lon, data.lat, c=col)
 #    plt.show()
+    track = read_tracks(inp_fld + inp_file_1, inp_fld + inp_file_2)
+    print(track.columns)
+    plt.figure()
+    plt.plot(track.lat1 - track.lat2)
+    plt.plot(track.lon1 - track.lon2)
+    plt.show()
+
+    '''
+    start_time = max(track_1.time[0], track_2.time[0])
+    end_time = min(track_1.time[track_1.shape[0] - 1], track_2.time[track_2.shape[0] - 1])
+    track_1 = track_1.between_time(start_time.time(), end_time.time())
+    track_2 = track_2.between_time(start_time.time(), end_time.time())
+    extention_2 = [__new_point(t, track_2) for t in track_1.time if t not in track_2.time]
+    for t in track_1.time:
+        if t in track_2.time:
+            continue
+        extention_2.append([t, 90, 360])
+    '''
